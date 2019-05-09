dbc4466d5d52849b2cc7856f39125366b75a3030
khoi <khoiracle@gmail.com>
2019-05-09 11:39:55 +0700

    add ToHumanReadableString to ReportPeriod

--- Toggl.Core/Models/ReportPeriod.cs
+++ Toggl.Core/Models/ReportPeriod.cs
@@ -2,7 +2,7 @@ namespace Toggl.Core.Models
 {
     public enum ReportPeriod
     {
-        Unknown,
+        Unknown = 0,
         LastMonth,
         LastWeek,
         Yesterday,
@@ -11,5 +11,33 @@ namespace Toggl.Core.Models
         ThisMonth,
         ThisYear,
         LastYear
+    }
+
+    public static class ReportPeriodExtensions
+    {
+        public static string ToHumanReadableString(this ReportPeriod p)
+        {
+            switch (p)
+            {
+                case ReportPeriod.LastMonth:
+                    return Resources.LastMonth;
+                case ReportPeriod.LastWeek:
+                    return Resources.LastWeek;
+                case ReportPeriod.Yesterday:
+                    return Resources.Yesterday;
+                case ReportPeriod.Today:
+                    return Resources.Today;
+                case ReportPeriod.ThisWeek:
+                    return Resources.ThisWeek;
+                case ReportPeriod.ThisMonth:
+                    return Resources.ThisMonth;
+                case ReportPeriod.ThisYear:
+                    return Resources.ThisYear;
+                case ReportPeriod.LastYear:
+                    return Resources.LastYear;
+                default:
+                    return Resources.Unknown;
+            }
+        }
     }
 }
