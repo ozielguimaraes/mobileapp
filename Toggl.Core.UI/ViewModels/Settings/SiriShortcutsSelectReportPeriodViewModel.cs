558ea7b463d396e0a82989876201cd30b77f7708
khoi <khoiracle@gmail.com>
2019-05-09 09:26:38 +0700

    add SiriShortcutsSelectReportPeriodViewModel

---
+++ Toggl.Core.UI/ViewModels/Settings/SiriShortcutsSelectReportPeriodViewModel.cs
@@ -0,0 +1,14 @@
+using MvvmCross;
+
+namespace Toggl.Core.UI.ViewModels.Settings
+{
+    [Preserve(AllMembers = true)]
+
+    public sealed class SiriShortcutsSelectReportPeriodViewModel : ViewModel
+    {
+        public SiriShortcutsSelectReportPeriodViewModel()
+        {
+
+        }
+    }
+}
