a60ecfdbf69d243b7baa0f9c78033b8d1db57bb8
khoi <khoiracle@gmail.com>
2019-05-09 09:42:14 +0700

    Add SiriShortcutsSelectReportPeriodViewController

---
+++ Toggl.iOS/ViewControllers/Settings/SiriShortcutsSelectReportPeriodViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace Toggl.iOS.ViewControllers.Settings
+{
+    public partial class SiriShortcutsSelectReportPeriodViewController : UIViewController
+    {
+        public SiriShortcutsSelectReportPeriodViewController() : base("SiriShortcutsSelectReportPeriodViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
